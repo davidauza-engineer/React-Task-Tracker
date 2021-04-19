3cd674b873b2caf26043cda49e41f11a8711a2b9
davidauza.engineer <davidauza01@gmail.com>
2021-04-18 23:17:46 -0500

    Add AddTask component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-  const clickHandler = () => {
-    console.log('Click');
-  }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' clickHandler={clickHandler} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} clickHandler={onAdd} />
     </header>
   )
 }
